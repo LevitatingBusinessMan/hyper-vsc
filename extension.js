78b1d5dbc669fb53fd3e2271ce5508565b3c49c3
Gamerein <reinrijn@gmail.com>
2018-07-07 15:09:57 +0200

    extension now able to be used as command

--- extension.js
+++ extension.js
@@ -4,8 +4,10 @@ const child_process = require('child_process');
 
 exports.activate = context => {
     let command = vscode.commands.registerCommand('extension.openHyper', file => {
-        console.log(file.path.substr(1));
-        child_process.exec(`hyper "${file.path.substr(1)}"`, {}, err => {
+
+        child_process.exec(`hyper "${file ? file.path.charAt(1).toUpperCase() + file.path.substring(2) : ''}"`, {
+            cwd: require('os').homedir()
+        }, err => {
             if (err) {
                 console.log("Hyper error: " + err);
                 vscode.window.showInformationMessage(err);
