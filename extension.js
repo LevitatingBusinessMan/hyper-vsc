1bc882fff3ae52c8974364ced6b3ab9df1abd182
Gamerein <reinrijn@gmail.com>
2018-07-07 14:50:09 +0200

    icon, link to marketplace

--- extension.js
+++ extension.js
@@ -6,7 +6,6 @@ exports.activate = context => {
     let command = vscode.commands.registerCommand('extension.openHyper', file => {
         console.log(file.path.substr(1));
         child_process.exec(`hyper "${file.path.substr(1)}"`, {}, err => {
-            console.log('Launched Hyper Terminal');
             if (err) {
                 console.log("Hyper error: " + err);
                 vscode.window.showInformationMessage(err);
